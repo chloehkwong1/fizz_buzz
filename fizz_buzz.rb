4586d3b7c66e57357d56f60ac2da0bd09965116c
Chloe <chloe.wong@madetech.com>
2020-09-24 16:56:15 +0100

    sliming to get input of 3 to return fizz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,6 @@
 def fizz_buzz(num)
+    if num == 3
+        return "fizz"
+    end
     num.to_s
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,19 +1,29 @@
 require './fizz_buzz'
 
-describe "When I fizzbuzz number 1" do
-    it "returns a string representing it" do
-        expect(fizz_buzz(1)).to eq("1")
-    end 
-end
+describe "testing behaviour for numbers not divisible by 3 or 5" do
+    context "When I fizzbuzz number 1" do
+        it "returns a string representing it" do
+            expect(fizz_buzz(1)).to eq("1")
+        end 
+    end
 
-describe "When I fizzbuzz number 2" do
-    it "returns a string representing it" do
-        expect(fizz_buzz(2)).to eq("2")
+    context "When I fizzbuzz number 2" do
+        it "returns a string representing it" do
+            expect(fizz_buzz(2)).to eq("2")
+        end
+    end
+
+    context "When I fizzbuzz number 4" do
+        it "returns a string representing it" do
+            expect(fizz_buzz(4)).to eq("4")
+        end
     end
 end
 
-describe "When I fizzbuzz number 4" do
-    it "returns a string representing it" do
-        expect(fizz_buzz(4)).to eq("4")
+describe "testing behaviour for numbers divisible by 3" do
+    context "When I fizzbuzz number 3" do
+        it "returns a 'fizz'" do
+            expect(fizz_buzz(3)).to eq("fizz")
+        end
     end
 end
